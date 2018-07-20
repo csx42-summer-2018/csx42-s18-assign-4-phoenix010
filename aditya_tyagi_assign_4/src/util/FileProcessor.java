d14c359f5f9ac25856cad6d2aef6049cc1d6579a
phoenix010 <aditya-tyagi@outlook.com>
2018-07-20 16:54:11 -0400

    Input file is ready

--- aditya_tyagi_assign_4/src/util/FileProcessor.java
+++ aditya_tyagi_assign_4/src/util/FileProcessor.java
@@ -1,5 +1,63 @@
 package util;
 
-public class FileProcessor {
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+//import java.io.FileNotFoundException;
+
+
+/**Method reads the input file from the user. and returns the string file back to the calling method
+ * 
+ * @param input.txt
+ * @return
+ * @throws IOException
+ */
+	
+public class FileProcessor {
+	
+	BufferedReader bufferedReader; 
+	
+	public BufferedReader getBufferedReader() {
+		return bufferedReader;
+	}
+
+	public void setBufferedReader(BufferedReader bufferedReader) {
+		this.bufferedReader = bufferedReader;
+	}
+
+
+	public FileProcessor(String input) {
+		InputStream fis = null;
+		try {
+			fis = new FileInputStream(input);
+		} catch (FileNotFoundException e) {
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+		this.setBufferedReader(br);
+	}
+
+	public String readLine() throws IOException {
+		String line;
+		try{
+		while((line = this.getBufferedReader().readLine()) != null) {
+			return line;
+		}
+		
+		}catch(IOException e) {
+			System.out.println("File Not Found");
+//			e.printStackTrsace();
+		}
+		return null;
+}
+	
 }
